e38bcff2e7a4a1801e2478cd1f68ee2838b63cfe
Anastasiia <iprastas@yandex.ru>
2025-03-17 13:07:32 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+class Program
+{
+    static string botToken = ;
+    static TelegramBotClient botClient = new TelegramBotClient(botToken);
+
+    static async Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
